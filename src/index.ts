e99048d7df80d67b1eb1426a88d83bead1666af7
NewtonMutugi <newtonmut197@gmail.com>
2024-10-21 14:33:52 +0300

    Refactor quotes in index.ts to use single quotes for consistency

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import bodyParser from "body-parser";
-import express, { Application, Request, Response } from "express";
+import bodyParser from 'body-parser';
+import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "Hello World" });
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Hello World' });
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000');
 });
