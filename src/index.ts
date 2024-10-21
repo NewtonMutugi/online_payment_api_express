cdba9a211867b4edc5eb4ffad29420d5294926f6
NewtonMutugi <newtonmut197@gmail.com>
2024-10-21 15:00:23 +0300

    Add error handling middleware and throw error for user not found

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import bodyParser from 'body-parser';
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 
 const app: Application = express();
 
@@ -7,7 +7,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
+  throw new Error('User not found');
   res.status(200).json({ message: 'Hello World' });
+});
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  res.status(400).json({ success: false, message: err.message });
 });
 
 app.listen(3000, () => {
